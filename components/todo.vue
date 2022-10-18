cfbc406a4910b5d18b8e48cb4476121c5de433dd
nopparat <tomlovetom20111@hotmail.com>
2022-10-18 11:30:17 +0700

    testVue

---
+++ components/todo.vue
@@ -0,0 +1,13 @@
+<div id="app">
+    <p v-if="isMe">Hey, I am here {{ message }}</p>
+  </div>
+  
+  <script>
+    const app = new Vue({
+      el: '#app',
+      data: {
+        message: 'Ahoy Vue!',
+        isMe: true
+      }
+    })
+  </script>
